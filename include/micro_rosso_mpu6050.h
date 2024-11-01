c5f29764c5688bd2bf790b55c33fc6563b6f6e9d
xopxe <xxopxe@gmail.com>
2024-11-01 11:40:18 -0300

    cleanup

--- include/micro_rosso_mpu6050.h
+++ include/micro_rosso_mpu6050.h
@@ -3,14 +3,14 @@
 
 #include <Wire.h>
 
-class ImuMPU6050 {
+class ImuMPU6050
+{
 public:
   ImuMPU6050();
-  static bool setup( TwoWire &wire = Wire,
-                     const char* topic_raw = "/imu/raw",
-                     const char* topic_temp = "/imu/temperature",
-                     timer_descriptor &timer_report = micro_rosso::timer_report
-  );
+  static bool setup(TwoWire &wire = Wire,
+                    const char *topic_raw = "/imu/raw",
+                    const char *topic_temp = "/imu/temperature",
+                    timer_descriptor &timer_report = micro_rosso::timer_report);
 };
 
-#endif  // __imu_mpu6050_h
+#endif // __imu_mpu6050_h
--- src/micro_rosso_mpu6050.cpp
+++ src/micro_rosso_mpu6050.cpp
@@ -1,6 +1,6 @@
-#define MPU6050_RANGE_G MPU6050_RANGE_4_G        // 2, 4, 8, 16 G
-#define MPU6050_RANGE_DEG MPU6050_RANGE_250_DEG  // 250, 500, 1000, 2000 deg/s
-#define MPU6050_BAND MPU6050_BAND_10_HZ          // 5 10 21 44 94 184 260
+#define MPU6050_RANGE_G MPU6050_RANGE_4_G       // 2, 4, 8, 16 G
+#define MPU6050_RANGE_DEG MPU6050_RANGE_250_DEG // 250, 500, 1000, 2000 deg/s
+#define MPU6050_BAND MPU6050_BAND_10_HZ         // 5 10 21 44 94 184 260
 
 #define IMU_TEMPERATURE_INTERVAL_MS 1000
 
@@ -26,32 +26,37 @@ static Adafruit_MPU6050 mpu;
 static unsigned long temperature_read_time_ms;
 static unsigned long temperature_topic_time_ms;
 
-#define RCCHECK(fn) \
-  { \
-    rcl_ret_t temp_rc = fn; \
-    if ((temp_rc != RCL_RET_OK)) { return false; } \
+#define RCCHECK(fn)              \
+  {                              \
+    rcl_ret_t temp_rc = fn;      \
+    if ((temp_rc != RCL_RET_OK)) \
+    {                            \
+      return false;              \
+    }                            \
   }
-#define RCNOCHECK(fn) \
-  { \
+#define RCNOCHECK(fn)       \
+  {                         \
     rcl_ret_t temp_rc = fn; \
-    (void)temp_rc; \
+    (void)temp_rc;          \
   }
 
-ImuMPU6050::ImuMPU6050() {
+ImuMPU6050::ImuMPU6050()
+{
 
   msg_imu.header.frame_id = micro_ros_string_utilities_set(msg_imu.header.frame_id, "imu_link");
 
   msg_imu.angular_velocity_covariance[0] = msg_imu.angular_velocity_covariance[4] = msg_imu.angular_velocity_covariance[8] = 0.05;
   msg_imu.linear_acceleration_covariance[0] = msg_imu.linear_acceleration_covariance[4] = msg_imu.linear_acceleration_covariance[8] = 0.05;
-  msg_imu.orientation_covariance[0] = -1;  // orientation not available from mpu6050
-
+  msg_imu.orientation_covariance[0] = -1; // orientation not available from mpu6050
 
   msg_temperature.temperature = -273.15;
 };
 
-static bool retrieve_raw() {
+static bool retrieve_raw()
+{
   sensors_event_t a, g, temp;
-  if (!mpu.getEvent(&a, &g, &temp)) {
+  if (!mpu.getEvent(&a, &g, &temp))
+  {
     return false;
   }
 
@@ -66,22 +71,27 @@ static bool retrieve_raw() {
   micro_rosso::set_timestamp(msg_imu.header.stamp);
 
   msg_temperature.temperature = temp.temperature;
-  //set_header_timestamp(msg_temperature.header);
+  // set_header_timestamp(msg_temperature.header);
   temperature_read_time_ms = now;
 
   return true;
 }
 
-static bool retrieve_temperature() {
+static bool retrieve_temperature()
+{
   unsigned long now = millis();
-  if (now - temperature_topic_time_ms > IMU_TEMPERATURE_INTERVAL_MS) {
+  if (now - temperature_topic_time_ms > IMU_TEMPERATURE_INTERVAL_MS)
+  {
     // if last reading too old, refresh
-    if (now - temperature_read_time_ms > IMU_TEMPERATURE_INTERVAL_MS) {
+    if (now - temperature_read_time_ms > IMU_TEMPERATURE_INTERVAL_MS)
+    {
       sensors_event_t a, g, temp;
-      if (!mpu.getEvent(&a, &g, &temp)) {
+      if (!mpu.getEvent(&a, &g, &temp))
+      {
         return false;
       }
-      if (msg_temperature.temperature != temp.temperature) {
+      if (msg_temperature.temperature != temp.temperature)
+      {
         return false;
       }
       msg_temperature.temperature = temp.temperature;
@@ -90,34 +100,41 @@ static bool retrieve_temperature() {
     micro_rosso::set_timestamp(msg_temperature.header.stamp);
     temperature_topic_time_ms = now;
     return true;
-  } else {
+  }
+  else
+  {
     return false;
   }
 }
 
-static void report_cb(int64_t last_call_time) {
-  if (retrieve_raw()) {
+static void report_cb(int64_t last_call_time)
+{
+  if (retrieve_raw())
+  {
     micro_rosso::set_timestamp(msg_imu.header.stamp);
     RCNOCHECK(rcl_publish(
-      &pdescriptor_imu.publisher,
-      &msg_imu,
-      NULL));
+        &pdescriptor_imu.publisher,
+        &msg_imu,
+        NULL));
   }
-  if (retrieve_temperature()) {
+  if (retrieve_temperature())
+  {
     micro_rosso::set_timestamp(msg_temperature.header.stamp);
     RCNOCHECK(rcl_publish(
-      &pdescriptor_temperature.publisher,
-      &msg_temperature,
-      NULL));
+        &pdescriptor_temperature.publisher,
+        &msg_temperature,
+        NULL));
   }
 }
 
-bool ImuMPU6050::setup( TwoWire &wire,
-                     const char* topic_raw,
-                     const char* topic_temp,
-                     timer_descriptor &timer_report ) {
-  D_println("setup imu_mpu6050");
-  if (!mpu.begin(MPU6050_I2CADDR_DEFAULT, &wire, 0)) {
+bool ImuMPU6050::setup(TwoWire &wire,
+                       const char *topic_raw,
+                       const char *topic_temp,
+                       timer_descriptor &timer_report)
+{
+  D_print("setup imu_mpu6050... ");
+  if (!mpu.begin(MPU6050_I2CADDR_DEFAULT, &wire, 0))
+  {
     return false;
   }
   mpu.setAccelerometerRange(MPU6050_RANGE_G);
@@ -126,18 +143,19 @@ bool ImuMPU6050::setup( TwoWire &wire,
 
   pdescriptor_imu.qos = QOS_DEFAULT;
   pdescriptor_imu.type_support =
-    (rosidl_message_type_support_t*)ROSIDL_GET_MSG_TYPE_SUPPORT(
-      sensor_msgs, msg, Imu);
+      (rosidl_message_type_support_t *)ROSIDL_GET_MSG_TYPE_SUPPORT(
+          sensor_msgs, msg, Imu);
   pdescriptor_imu.topic_name = topic_raw;
   micro_rosso::publishers.push_back(&pdescriptor_imu);
 
   pdescriptor_temperature.qos = QOS_DEFAULT;
-  pdescriptor_temperature.type_support = (rosidl_message_type_support_t*)
-    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature);
+  pdescriptor_temperature.type_support = (rosidl_message_type_support_t *)
+      ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature);
   pdescriptor_temperature.topic_name = topic_temp;
   micro_rosso::publishers.push_back(&pdescriptor_temperature);
 
   timer_report.callbacks.push_back(&report_cb);
 
+  D_println("done.");
   return true;
 }
